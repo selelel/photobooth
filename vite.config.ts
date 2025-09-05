8348d034f80dc88f80b3040154d1627132e9bac3
selelel <janrusselgorembalem4@gmail.com>
2025-09-05 17:17:46 +0800

    feat: make it Progressive Web App

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,39 @@ export default defineConfig({
     tanstackRouter({
     target: 'react',
     autoCodeSplitting: true,
-  }), react(), tailwindcss()],
+  }), 
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Photobooth',
+      short_name: 'MyApp',
+      description: 'JJ photobooth',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      // icons: [
+      //   {
+      //     src: '/pwa-192x192.png',
+      //     sizes: '192x192',
+      //     type: 'image/png',
+      //   },
+      //   {
+      //     src: '/pwa-512x512.png',
+      //     sizes: '512x512',
+      //     type: 'image/png',
+      //   },
+      //   {
+      //     src: '/pwa-512x512.png',
+      //     sizes: '512x512',
+      //     type: 'image/png',
+      //     purpose: 'any maskable',
+      //   },
+      // ],
+    },
+  }),
+  react(), 
+  tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
